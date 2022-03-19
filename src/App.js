5727d038fa9697396aa0e2604f7ec769a86a4aca
adder <59740233+sweet-little-adder@users.noreply.github.com>
2022-03-19 19:27:07 +0800

    ready to publish

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import logo from "./logo.svg";
 import "./App.css";
 import styled from "styled-components";
 
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        const Button = styled.button``
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a mirror of <code>killergf</code>
+          <br></br>
+          killerbf
         </p>
+        <Button>View On OpenSea</Button>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this site is created with react ;(
         </a>
       </header>
     </div>
