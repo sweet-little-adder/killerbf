fc386f2f87d0983646b1fcee7398a1b2f95cd43d
sweet-little-adder <orioncheetah@gmail.com>
2022-03-19 18:24:21 +0800

    importing styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        const Button = styled.button``
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
